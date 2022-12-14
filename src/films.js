a753b7e0db85fa51e6aa7155d37261a1fb99e23f
Hector Ramirez Garcia <hectorfx130@gmail.com>
2022-12-14 10:42:47 +0100

    Ejercicio 3

--- src/films.js
+++ src/films.js
@@ -1,45 +1,50 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result = array.map(peliculas => peliculas.director);
+  const result = array.map(peliculas => peliculas.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  const result = array.filter(dire => dire.director = director);
+  const result = array.filter(dire => dire.director == director);
   console.log("EXERCICE 2 ->", result);
   return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  const initialValue = 0.00;
+  let result = array.filter(dire => dire.director == director).reduce((sum, currentValue) => sum + currentValue.score, initialValue);
+  let loops = array.filter(dire => dire.director == director).length;
+  result = result/loops;
+  console.log("EXERCICE 3 ->", result);
+  return result;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  return 'undefined';
 }
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {
-
+  return 'undefined';
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory() {
-
+  return 'undefined';
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
-
+  return 'undefined';
 }
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+  return 'undefined';
 }
 
 
