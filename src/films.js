19f582a86daaf4f760f27f87b94e4b1f9059ffb7
Hector Ramirez Garcia <hectorfx130@gmail.com>
2022-12-16 18:36:34 +0100

    Ejercicio 7 y 8

--- src/films.js
+++ src/films.js
@@ -62,18 +62,58 @@ function orderByYear(array) {
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-  return 'undefined';
+function moviesAverageByCategory(array, category) {
+  const copyArray = [...array];
+  const initialValue = 0.00; 
+  let result = copyArray.filter(cate => cate.genre == category).reduce((sum, currentValue) => sum + currentValue.score, initialValue);
+  let loops = copyArray.filter(cate => {
+    if(cate.score != null && cate.score != ''){
+      return cate.genre == category}
+    }).length;
+  result = result/loops;
+  result = Math.round(result * 100)/100;
+  console.log("EXERCICE 6 ->", result);
+  return result;
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-  return 'undefined';
+function hoursToMinutes(array) {
+  const copyArray = [...array];
+  const result = copyArray.map(peliculas => {
+    let hours = 0;
+    let min = 0;
+    if(parseInt(peliculas.duration[1]) == 'h') {
+      hours = peliculas.duration[0];
+    } else {
+      hours = peliculas.duration[0] + peliculas.duration[1];
+    }
+    hours = parseInt(hours);
+    if(peliculas.duration.length > 2){
+      if(parseInt(peliculas.duration.charAt(3, 5)) != NaN) {
+        min = peliculas.duration.slice(3, 5);
+      } else {
+        min = peliculas.duration[3];
+      }
+    }
+    min = parseInt(min);
+    let hoursToMin = hours*60;
+    return { duration : min += hoursToMin}
+  });
+  console.log('EXERCICE 7 ->', result);
+  return result;
 }
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  return 'undefined';
+function bestFilmOfYear(array, year) {
+  const copyArray = [...array];
+  const find_best_movie = copyArray.sort((a, b) => {
+    if (a.year == year) {
+      return a.score - b.score;
+    }
+  }).reverse();
+  const result_array = [find_best_movie[0]];
+  console.log('EXERCICE 8 ->', result_array);
+  return result_array;
 }
 
 
